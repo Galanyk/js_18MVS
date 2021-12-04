a86375c71b3ab7636fa698e20d64ee28f0a4ec93
Svetlana Galanyk <galanyksa1@gmail.com>
2021-12-04 10:52:21 +0200

    id

--- main/MVC/view/TodoListView.js
+++ main/MVC/view/TodoListView.js
@@ -7,11 +7,11 @@ class TodoListView {
         BUTTON_SAVE_NEW_USER: 'button-saveNewUser'
     };
     static ID = {
-        BUTTON_SAVE_ID: '#button-save',
-        USER_NAME_ID: '#user-name',
-        NEW_USER_NAME_ID: '#new-user-name',
-        NEW_USER_ADDRESS_ID: '#new-user-address',
-        NEW_USER_PHONE_ID: '#new-user-phone',
+        BUTTON_SAVE_ID: 'button-save',
+        USER_NAME_ID: 'user-name',
+        NEW_USER_NAME_ID: 'new-user-name',
+        NEW_USER_ADDRESS_ID: 'new-user-address',
+        NEW_USER_PHONE_ID: 'new-user-phone',
     }
 
     static INPUT = 'input'
@@ -66,14 +66,14 @@ class TodoListView {
 
     createEditHtml(user) {
         return `<li id=${user.id} class="${TodoListView.ITEM}" contenteditable="true">Name:${user.name}</li>
-        <li id=${user.id}  class=" ${TodoListView.ITEM_ADDRESS}" contenteditable="true">Address: ${user.address.city}</li>
+        <li id=${user.id}  class=" ${TodoListView.ITEM_ADDRESS}" contenteditable="true">Address:${user.address.city}</li>
         <li id=${user.id}  class=" ${TodoListView.ITEM_PHONE}" contenteditable="true">Phone: ${user.phone}</li>
-        <button id="${TodoListView.ID.BUTTON_SAVE_ID}" class="${TodoListView.BUTTON_CLASS.BUTTON_SAVE}">Save</button>`
+        <button id="#${TodoListView.ID.BUTTON_SAVE_ID}" class="${TodoListView.BUTTON_CLASS.BUTTON_SAVE}">Save</button>`
     };
 
     createUserCreateContainer($container) {
         const el = $(`<div class="${TodoListView.CONTAINER_INPUT}">
-        <input id="user-name" class="${TodoListView.INPUT}" placeholder="Name">
+        <input id="${TodoListView.ID.USER_NAME_ID}" class="${TodoListView.INPUT}" placeholder="Name">
         <button id="button-input" class="${TodoListView.BUTTON_CLASS.BUTTON_NEW_USER}">Enter</button></div>`)
             .on('click', `.${TodoListView.BUTTON_CLASS.BUTTON_NEW_USER}`, (e) => this.onEnterClick(e));
         $container.prepend(el);
@@ -81,9 +81,9 @@ class TodoListView {
 
     createNewUserContainer($container) {
         this.newUser = $(`<div class="${TodoListView.CONTAINER_USER_OPEN}"> 
-        <p>Name:</p><input id="new-user-name" class=" ${TodoListView.ITEM}"> 
-        <p>Address:</p><input id="new-user-address" class=" ${TodoListView.ITEM_ADDRESS}">
-        <p>Phone:</p><input id="new-user-phone" class=" ${TodoListView.ITEM_PHONE}"> 
+        <p>Name:</p><input id="${TodoListView.ID.NEW_USER_NAME_ID}" class=" ${TodoListView.ITEM}"> 
+        <p>Address:</p><input id="${TodoListView.ID.NEW_USER_ADDRESS_ID}" class=" ${TodoListView.ITEM_ADDRESS}">
+        <p>Phone:</p><input id="${TodoListView.ID.NEW_USER_PHONE_ID}" class=" ${TodoListView.ITEM_PHONE}"> 
         <button id="button-saveNewUser" class="${TodoListView.BUTTON_CLASS.BUTTON_SAVE_NEW_USER}">Save</button></div>`);
         $($container).prepend(this.newUser);
     };
@@ -120,19 +120,18 @@ class TodoListView {
     onEnterClick(e) {
         if (this.isEnter && $(TodoListView.INPUT).val().length > 0) {
             this.createNewUserContainer(this.$ListContainerEl);
-            $(`${TodoListView.ID.NEW_USER_NAME_ID}`).val($(TodoListView.INPUT).val());
-            $(`${TodoListView.ID.USER_NAME_ID}`).val('');
+            $(`#${TodoListView.ID.NEW_USER_NAME_ID}`).val($(TodoListView.INPUT).val());
+            $(`#${TodoListView.ID.USER_NAME_ID}`).val('');
             this.isEnter = false;
         };
     };
 
     onSaveNewUser(e) {
-        const name = $(`${TodoListView.ID.NEW_USER_NAME_ID}`).val();
+        const name = $(`#${TodoListView.ID.NEW_USER_NAME_ID}`).val();
         const address = {
-            city: $(`${TodoListView.ID.NEW_USER_ADDRESS_ID}`).val()
+            city: $(`#${TodoListView.ID.NEW_USER_ADDRESS_ID}`).val()
         };
-        console.log('Address: ', address.city);
-        const phone = $(`${TodoListView.ID.NEW_USER_PHONE_ID}`).val();
+        const phone = $(`#${TodoListView.ID.NEW_USER_PHONE_ID}`).val();
         this.newUser = { name, address, phone }
         this.options.onAddNewUser(this.newUser);
         this.isEnter = true;
